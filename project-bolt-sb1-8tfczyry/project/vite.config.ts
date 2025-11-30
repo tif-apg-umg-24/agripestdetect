002c806b69a29d8a45d8cffbb4b3e9365f49fc56
TIF-APG-UMG-24 <teknikinformatikapagi24@gmail.com>
2025-11-30 18:49:42 +0700

    Delete project-bolt-sb1-8tfczyry/project directory

--- project-bolt-sb1-8tfczyry/project/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/agripestdetect/',   // <-- ganti sesuai nama repo GitHub
-  plugins: [react()],
-});
